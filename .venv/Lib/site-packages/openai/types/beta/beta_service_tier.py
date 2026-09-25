23199e848414c8f518bf5dfd9202f610d2771d7b
zkipi <zkip.wikan@gmail.com>
2026-09-25 07:18:31 +0200

    Delete .venv directory

--- .venv/Lib/site-packages/openai/types/beta/beta_service_tier.py
+++
@@ -1,8 +0,0 @@
-# File generated from our OpenAPI spec by Castiron. See CONTRIBUTING.md for details.
-
-from typing import Optional
-from typing_extensions import Literal, TypeAlias
-
-__all__ = ["BetaServiceTier"]
-
-BetaServiceTier: TypeAlias = Optional[Literal["auto", "default", "flex", "scale", "priority", "fast", "ultrafast"]]
--- .venv/Lib/site-packages/openai/types/beta/realtime/realtime_connect_params.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from typing_extensions import Required, TypedDict
-
-__all__ = ["RealtimeConnectParams"]
-
-
-class RealtimeConnectParams(TypedDict, total=False):
-    model: Required[str]
